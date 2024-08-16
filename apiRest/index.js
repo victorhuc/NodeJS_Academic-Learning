23968b0b2af1936e73df60b0b6d6486abb07a668
VICTOR HUGO COSTA CORREA <victor_h_correa@estudante.sesisenai.org.br>
2024-08-15 21:46:29 -0300

    adicionando apiRest

--- apiRest/index.js
+++ apiRest/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import colecaoUf from './dados/dados.js';
+import colecaoUf from './dados.js';
 
 const app = express();
 
@@ -7,8 +7,42 @@ app.get('/ufs', (req, res) => {
     res.json(colecaoUf)
 });
 
-app.listen(8080, () => {
-    console.log('Servidor rodando na porta 8080')
+// app.get('/ufs/teste', (req, res) => {
+//     res.send({"teste": "teste"})
+// }
+// );
+
+app.get('/ufs/:iduf', (req, res) => {
+    const idUF = parseInt(req.params.iduf);
+    // const uf = colecaoUf.find(u => u.id === idUF);
+    // res.json(uf)
+    let mensagemErro = '';
+    let uf;
+
+    if (!(isNaN(idUF))) {
+        uf = colecaoUf.find(u => u.id === idUF);
+        if(!uf){
+            mensagemErro = 'UF não encontrada';
+        }
+    }else{
+        mensagemErro = 'Requisição inválida';
+    }
+
+    if (uf) {
+        res.json(uf);
+    }else{
+        res.status(404).send({ "erro": mensagemErro });
+    }
+});
+
+// app.get('ufs/:iduf', (req, res) => {
+//     const idUF = req.params.iduf;
+//     res.send({"teste" : `${idUF}`});
+
+// });
+
+app.listen(8000, () => {
+    console.log('Servidor iniciado na porta 8000')
 });
 
 
